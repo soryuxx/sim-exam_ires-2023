2467973b206e8643713d182eca14f0c1be494251
Ana Potochina <potochina1@gmail.com>
2023-07-24 12:48:59 +0200

    first commit

--- src/main/java/it/ires/exam/PhoneApp.java
+++ src/main/java/it/ires/exam/PhoneApp.java
@@ -1,13 +1,13 @@
 package it.ires.exam;
 
-import it.ires.exam.dataobject.OperatorPlan;
-import it.ires.exam.dataobject.SimCard;
+import it.ires.exam.Sim.Sim;
 
 public class PhoneApp {
 
   public static void main(String[] args) {
-    OperatorPlan plan = new OperatorPlan("standard", 15, 15);
-    SimCard sim = new SimCard("+39 123 4567 890", plan, 100);
+    Sim sim = new Sim("3278151315", "wind");
+
+
   }
 
 }
